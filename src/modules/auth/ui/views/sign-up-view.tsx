0326096334260f3a47b52d30cb729ad42214b4a5
pratham2505 <p.patel034@umb.edu>
2025-08-20 19:48:44 -0400

    04:Updated-Authenticaion-UI

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -204,14 +204,14 @@ export const SignUpView = () => {
                     </Form>
 
                     <div className="bg-radial from-green-500 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
-                        <img src = "/logo.svg" alt="Image" className="h-[92px] w-[92px]"/>
+                        <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]"/>
                         <p className="text-2xl font-semibold text-white">
                             Meet.AI
                         </p>
                     </div>
                 </CardContent>
             </Card>
-            <div className="text-muter-foregrouyynd *:[a]hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+            <div className="text-muter-foreground *:[a]hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
                   By clicking continue, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacyt Policy</a>
             </div>
         </div>
