253da271c56a7d93cd48945450a7ef08c721174c
pratham2505 <p.patel034@umb.edu>
2025-09-08 13:50:35 -0400

    13:Agents-Filters

--- src/app/(dashboard)/agents/page.tsx
+++ src/app/(dashboard)/agents/page.tsx
@@ -1,10 +1,12 @@
 import { auth } from "@/lib/auth";
+import { loadSearchParams } from "@/modules/agents/params";
 import { AgentsListHeader } from "@/modules/agents/ui/components/agents-list-header";
 import { AgentsView, AgentsViewError, AgentsViewLoading } from "@/modules/agents/ui/views/agents-view"
 import { getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { SearchParams } from "nuqs";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -18,7 +20,13 @@ import { ErrorBoundary } from "react-error-boundary";
 // This is a great way to use the power of React Query and TRPC together
 // to build fast and SEO friendly applications with Next.js 13
 
-const Page = async () => {
+interface Props {
+    searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
+    const filters = await loadSearchParams(searchParams);
+
     const session = await auth.api.getSession ({
         headers: await headers(),
     });
@@ -28,7 +36,9 @@ const Page = async () => {
     }
 
     const queryClient = getQueryClient();
-    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions());
+    void queryClient.prefetchQuery(trpc.agents.getMany.queryOptions({
+        ...filters
+    }));
 
     return (
         <>
