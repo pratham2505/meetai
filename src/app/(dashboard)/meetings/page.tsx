43fccafa8f418b127d750cea4616db2d7aaa1739
pratham2505 <p.patel034@umb.edu>
2025-09-17 11:34:09 -0400

    19:Meetings-Filters

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -7,9 +7,16 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
+import { loadSearchParams } from "@/modules/meetings/params";
+import type { SearchParams } from "nuqs/server";
 
+interface Props {
+    searchParams: Promise<SearchParams>;
+}
 
-const Page = async () => {
+const Page = async ({ searchParams }: Props) => {
+    const filters = await loadSearchParams(searchParams);
+
     const session = await auth.api.getSession ({
         headers: await headers(),
     });
@@ -20,7 +27,9 @@ const Page = async () => {
 
     const queryClient = getQueryClient();
     void queryClient.prefetchQuery(
-        trpc.meetings.getMany.queryOptions({})
+        trpc.meetings.getMany.queryOptions({
+            ...filters,
+        })
     );
 
     return (
